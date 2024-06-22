4382c0ca2304e1484175bb8f8d9b4aac7b65eff8
harinath-az <123477960+harinath-az@users.noreply.github.com>
2024-06-22 11:36:12 -0500

    checking

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello, World!")
-    # The above function is used to print what ever is written inside it
+    # The above function is used to print what ever is written inside it.
 
 if __name__ == "__main__":
     main()
