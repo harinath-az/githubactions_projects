a09222b885bdb1658246f185b99e0a86c19f5fad
harinath-az <123477960+harinath-az@users.noreply.github.com>
2024-06-22 11:48:19 -0500

    Delete test_app.py

--- test_app.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from app import main
-
-class TestApp(unittest.TestCase):
-    def test_main(self):
-        self.assertIsNone(main())
-
-if __name__ == "__main__":
-    unittest.main()
